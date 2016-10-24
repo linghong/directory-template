242cb6a8ffdd7d2806a65536a13d1cfd64f191d6
linghong <linghong_chen@hotmail.com>
2016-10-24 17:25:43 -0400

    reorganized the style sheet.

--- app/app.component.js
+++ app/app.component.js
@@ -34,7 +34,7 @@ var AppComponent = (function () {
         core_1.Component({
             selector: 'my-app',
             template: "\n  <h1>{{title}}</h1>\n \n  <h2> My Heros</h2>\n  <ul class=\"items\">\n\t  <li *ngFor=\"let item of items\" (click)=\"onSelect(item)\" [class.selected]=\"item === selectedItem\">\n\t  \t<span class=\"badge\">{{item.id}}</span>{{item.name}}\n\t  </li>\n  </ul>\n  <item-detail [item]=\"selectedItem\"></item-detail>\n  ",
-            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .items {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .items li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .items li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .items li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .items .text {\n    position: relative;\n    top: -3px;\n  }\n  .items .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
+            styleUrls: ['app/style.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
